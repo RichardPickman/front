bc4f986d47615a9fa22bf6e0cbe5137fc9c6aad1
RichardPickman <drezzerock@gmail.com>
2023-06-03 14:09:44 +0200

    rearrange input attributes

--- src/components/InputWithLabel/index.tsx
+++ src/components/InputWithLabel/index.tsx
@@ -6,12 +6,13 @@ interface Props {
 
 export const InputWithLabel = ({ id, type, onChange }: Props) => (
     <input
+        id={id}
         min={1}
         required
         type={type}
         autoComplete="off"
         placeholder="Provide a value"
         onChange={(event) => onChange(event.target.value)}
-        className="px-2 py-1 border rounded outline-none" id={id}
+        className="px-2 py-1 border rounded outline-none"
     />
 );
