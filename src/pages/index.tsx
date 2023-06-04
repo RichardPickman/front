ea30087cf7300a6a56f9b6919c80752b6d76aae7
RichardPickman <drezzerock@gmail.com>
2023-06-04 12:51:35 +0200

    change Link wrapper to router hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,13 @@ import { Layout } from '@/components/Layout';
 import { BoxWithCheckbox } from '@/components/BoxWithCheckbox';
 import { Item } from '@/components/Item';
 import { useProducts } from '@/hooks/useProducts';
+import { useRouter } from 'next/router';
 
 
 export default function Home({ API_URL }: { API_URL: string }) {
   const [products, isError, getProducts, deleteProducts] = useProducts(API_URL);
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const router = useRouter();
 
   const onCheckboxClick = (id: string) =>
     selectedItems.includes(id)
@@ -26,34 +28,30 @@ export default function Home({ API_URL }: { API_URL: string }) {
   }, [])
 
   return (
-    <main className="flex justify-center">
-      <Layout>
-        <Head>
-          <title>Home</title>
-          <meta name="Home page of the best e commerce web site that human kind ever known" />
-        </Head>
-        <Header>
-          <h1>Product List</h1>
-          <div className="flex gap-2">
-            <Link href={'add-product'}>
-              <Button value="ADD" onClick={() => { }} />
-            </Link>
-            <Button value="MASS DELETE" onClick={onDelete} />
-          </div>
-        </Header>
-        <div className="grid grid-cols-4 gap-4">
-          {isError &&
-            <p className="text-red-500">
-              Error occured while getting a products! <span className="text-sky-500 underline" onClick={() => getProducts()}>Try again?</span>
-            </p>}
-          {products && products.map((item) => (
-            <BoxWithCheckbox key={item.sku} id={item.sku} isActive={selectedItems.includes(item.sku)} onChange={onCheckboxClick}>
-              <Item {...item} />
-            </BoxWithCheckbox>
-          ))}
+    <Layout>
+      <Head>
+        <title>Home</title>
+        <meta name="Home page of the best e commerce web site that human kind ever known" />
+      </Head>
+      <Header>
+        <h1>Product List</h1>
+        <div className="flex gap-2">
+          <Button value="ADD" onClick={() => router.push('/add-product')} />
+          <Button value="MASS DELETE" onClick={onDelete} />
         </div>
-      </Layout>
-    </main>
+      </Header>
+      <div className="grid grid-cols-4 gap-4">
+        {isError &&
+          <p className="text-red-500">
+            Error occured while getting a products! <span className="text-sky-500 underline" onClick={() => getProducts()}>Try again?</span>
+          </p>}
+        {products && products.map((item) => (
+          <BoxWithCheckbox key={item.sku} id={item.sku} isActive={selectedItems.includes(item.sku)} onChange={onCheckboxClick}>
+            <Item {...item} />
+          </BoxWithCheckbox>
+        ))}
+      </div>
+    </Layout>
   )
 }
 
