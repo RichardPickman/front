752f7f7742a7836816178ad3914921711bbe8392
RichardPickman <drezzerock@gmail.com>
2023-06-06 14:36:39 +0200

    BoxWithCheckbox revisited due to strange tests

--- src/components/BoxWithCheckbox/index.tsx
+++ src/components/BoxWithCheckbox/index.tsx
@@ -4,14 +4,12 @@ import { Card } from "../Card";
 interface BoxProps {
     id: string,
     children: ReactNode,
-    onChange: (id: string) => void,
-    isActive: boolean,
 }
 
-export const BoxWithCheckbox = ({ id, onChange, children, isActive }: BoxProps) => (
+export const BoxWithCheckbox = ({ id, children }: BoxProps) => (
     <Card>
-        <div className="flex flex-col gap-2 py-4 px-2 m-w-max delete-checkbox">
-            <input className="self-start" checked={isActive} type="checkbox" onChange={() => onChange(id)} />
+        <div className="flex flex-col gap-2 py-4 px-2 m-w-max">
+            <input id={id} className="self-start delete-checkbox" type="checkbox" />
             {children}
         </div>
     </Card>
