3ea982d47ccb0ee890daf7a8e736eb484af411e0
RichardPickman <drezzerock@gmail.com>
2023-06-07 16:24:15 +0200

    remove delete logic due to its presenting in hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,25 +11,11 @@ import { useProducts } from '@/hooks/useProducts';
 
 const Home = ({ API_URL }: { API_URL: string }) => {
     const [products, isError, getProducts, deleteProducts] = useProducts(API_URL);
-    const [isDelete, setDelete] = useState(false);
     const router = useRouter();
-
-    const onDelete = () => setDelete(true);
 
     useLayoutEffect(() => {
         getProducts();
     }, []);
-
-    useEffect(() => {
-        if (isDelete) {
-            const allDeleteInputs = document.querySelectorAll('.delete-checkbox') as NodeListOf<HTMLInputElement>;
-            const checked = Array.from(allDeleteInputs).filter(input => input.checked).map(item => item.id);
-
-            deleteProducts(checked);
-
-            setDelete(false);
-        }
-    }, [isDelete, deleteProducts])
 
     return (
         <Layout>
@@ -41,10 +27,10 @@ const Home = ({ API_URL }: { API_URL: string }) => {
                 <h1>Product List</h1>
                 <div className="flex gap-2">
                     <Button value="ADD" onClick={() => router.push('/add-product')} />
-                    <Button value="MASS DELETE" onClick={onDelete} />
+                    <Button value="MASS DELETE" onClick={() => deleteProducts()} />
                 </div>
             </Header>
-            <div className="grid grid-cols-4 gap-4">
+            <div className="grid grid-cols-4 w-full gap-4">
                 {isError &&
                     <p className="text-red-500">
                         Error occured while getting a products! <span className="text-sky-500 underline" onClick={() => getProducts()}>Try again?</span>
